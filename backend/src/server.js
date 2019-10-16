17b2f067e433cc3beadfeea80ea5c50d9c69fad5
Paulo Modesto <paulo.jmodesto@hotmail.com>
2019-10-15 23:52:32 -0300

    uploads

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,47 +1,18 @@
 const express = require('express');
-const router = require('./router');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const pessoa = require('./routes/pessoas');
+
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }))
 
-function execSQLQuert(sqlQry, res){
-    const connection = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : '',
-        database : 'rest_in_fun' 
-    });
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
 
-    // Conecsao ao Mysql
-    connection.query(sqlQry, function(error, results, fields){
-        if(error)
-            res.json(error)
-        else
-            res.json(results)
-        connection.end();
-        console.log('executou');
-    })
-}
+app.use('/pessoas', pessoa);
 
-app.get('/', (req, res) => {
-    res.send('Hello Wolrd');
-});
-
-app.get('/pessoas',(req, res)=> {
-    execSQLQuert('SELECT * FROM pessoa', res);
-});
-
-app.get('/pessoas/:id?',(req, res)=> {
-    let filter = ''
-    if(req.param.id) filter = ' WHERE ID=' + parseInt(req.params.id);
-    execSQLQuert('SELECT * FROM pessoa' + filter, res);
-});
-
-
-
-//app.use(router);
 app.listen(8080);
 
 // export the instanc of express
