385355a582c8531d199f0ab21f9ed9d456fde248
Paulo Modesto <paulo.jmodesto@hotmail.com>
2019-10-16 22:52:08 -0300

    criacao do arquivo aluguel

---
+++ backend/src/routes/aluguel.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db')
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+
+router.get('/',(req,res)=> {
+    var sql = "SELECT * FROM aluguel";
+    db.query(sql,(err, rows, fields) => {
+        if(err){
+            res.status(500).send({ error: "something failed !"})
+        }
+        res.json(rows);
+    })
+})
+
+router.get('/:id',(req,res)=>{
+    var id = req.params.id;
+    var sql = `SELECT * FROM aluguel WHERE id_servico=${id}`;
+    db.query(sql,(err, rows, fields) => {
+        if(err) {
+            res.status(500).send({ error: "something failed !"})
+        }
+        res.json(rows);
+    })
+})
+
+module.exports = router;
